07e6fb87242c4873352fe90788deacb646a1ffd9
Amitkochrekar <123859808+Amitkochrekar@users.noreply.github.com>
2023-04-13 16:51:05 +0530

    Update ec2-creation.tf

--- terraformscripts/ec2-creation.tf
+++ terraformscripts/ec2-creation.tf
@@ -1,17 +1,56 @@
-provider "aws"{
-  region = "ap-south-1"
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "4.62.0"
+    }
+  }
 }
 
-
-resource "aws_instance" "AWSInstance"{
-    ami = "ami-03c4dbdfc36515b16"
-	instance_type = "t2.micro"
-	key_name = "amitkey"
-	security_groups = ["launch-wizard-48"]
-	tags = {
-	 Name = "terraform"
-		
-	}
+provider "aws" {
+  # Configuration options
+region = "ap-south-1"
+}
+resource "aws_instance" "demoec2" {
+ami = "ami-0fdea1353c525c182"
+instance_type = "t2.micro"
+key_name = "amitkey"
+vpc_security_group_ids = [aws_security_group.tomcatserversg.id]
+tags = {
+  "Name" = "tomcatservers"
 }
 
+}
+resource "aws_security_group" "tomcatserversg" {
+  name = "tomcatserversg"
+  description = "Allow ssh http tomcat traffic"
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "Allow ssh port"
+  }
+    ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "Allow http port"
+  }
+    ingress {
+    from_port = 8080
+    to_port = 8080
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "Allow tomcat port"
+  }
+    egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "Allow ssh port"
+  }
+}
 
